423753864ca0269ce8260a5a7682a9e036d948c9
Mikael-UofA <lnineza@ualberta.ca>
2024-04-19 21:04:42 -0600

    Started work on HomeFragment UI. Note that is is just a placeholder and will be improved on in the future

---
+++ src/app/src/main/java/com/example/redditbot/AddSubredditFragment.java
@@ -0,0 +1,31 @@
+package com.example.redditbot;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class AddSubredditFragment extends Fragment {
+    public AddSubredditFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_subreddit, container, false);
+    }
+}
