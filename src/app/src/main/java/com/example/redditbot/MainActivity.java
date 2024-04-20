fed05f646a4b5aa524796abdc4efabedcb056898
Mikael-UofA <lnineza@ualberta.ca>
2024-04-19 19:38:09 -0600

    Completed the bottom navigation bar

--- src/app/src/main/java/com/example/redditbot/MainActivity.java
+++ src/app/src/main/java/com/example/redditbot/MainActivity.java
@@ -1,24 +1,61 @@
 package com.example.redditbot;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.redditbot.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ChangeFragment(new HomeFragment());
+
+        binding.BottomNavView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                ChangeFragment(new HomeFragment());
+            } else if (id == R.id.nav_account) {
+                ChangeFragment(new HomeFragment());
+            } else if (id == R.id.nav_settings) {
+                ChangeFragment(new SettingsFragment());
+            }
+
+            return true;
+        });
+    }
+
+    /**
+     * Use this method to display the fragment that is passed
+     * as an argument
+     *
+     * @param fragment The fragment we want to display.
+     */
+    private void ChangeFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
